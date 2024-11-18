a7ab5e3ceeb3b78e1a228f6d8e7ddfbf4382f74e
Jaden F <72635494+JadenF404@users.noreply.github.com>
2024-11-18 12:14:30 -0500

    fixed validator>

--- app/src/main/java/com/example/lab8/PasswordValidator.java
+++ app/src/main/java/com/example/lab8/PasswordValidator.java
@@ -7,13 +7,18 @@ public class PasswordValidator {
 
         boolean hasUpperCase = false;
         boolean hasLowerCase = false;
+        boolean hasNumber = false;
+
+        if (password.length() < 10) return false;
 
         for (char c : password.toCharArray()) {
             if (Character.isUpperCase(c)) hasUpperCase = true;
             if (Character.isLowerCase(c)) hasLowerCase = true;
+            if (Character.isDigit(c)) hasNumber = true;
+
 
             // If both conditions are met, the password is valid
-            if (hasUpperCase && hasLowerCase) return true;
+            if (hasUpperCase && hasLowerCase && hasNumber) return true;
         }
         return false;
     }
